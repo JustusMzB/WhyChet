cda5cfe8fb62e0ac71aa041c472a988b30b6256d
Justus MzB <ju@mzb.de>
2020-12-11 16:33:56 +0100

    Experimental Gui, not finished. Rest is fully functional.

--- WhyChet/WhyServer/src/main/java/de/JRoth/WhyServer/Main.java
+++ WhyChet/WhyServer/src/main/java/de/JRoth/WhyServer/Main.java
@@ -7,23 +7,21 @@ import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
+    Scene root;
+    static Server server;
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(this.getClass().getResource("/ServerMainScene.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        this.root = new Scene(root, 300, 275);
+        primaryStage.setScene(this.root);
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        Server myServer = new Server(1969);
-        myServer.start();
-        ServerOrderService orderService = new ConsoleServerController(myServer);
-        while (!orderService.closeSignal()) {
-            orderService.instruct();
-        }
+        Main.server = new Server(1969);
+        Main.server.start();
         launch(args);
-
     }
 }
--- WhyChet/WhyServer/src/main/java/de/JRoth/WhyServer/ServerMainSceneCtrl.java
+++ WhyChet/WhyServer/src/main/java/de/JRoth/WhyServer/ServerMainSceneCtrl.java
@@ -1,4 +1,42 @@
 package de.JRoth.WhyServer;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+
+
 public class ServerMainSceneCtrl {
+    @FXML
+    Tab tbUsers;
+    @FXML
+    VBox vbxUsers;
+    @FXML
+    Button btnDummy;
+
+    @FXML
+    void addDummy(ActionEvent event) {
+        Label label = new Label();
+        label.setText("HelloDummy");
+        try {
+            HBox uv = FXMLLoader.load(getClass().getResource("/UserView.fxml"));
+            vbxUsers.getChildren().add(uv);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void closeServer(ActionEvent event){
+        Main.server.terminate();
+    }
 }
